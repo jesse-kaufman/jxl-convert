28aab5c7739a3ac5487328aaf33cbd953762b507
Jesse Kaufman <jesse@jessekaufman.com>
2024-10-17 13:32:38 -0500

    Rework config.js and move to config folder

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+/**
+ * Configuration
+ */
+
+import path from "path";
+
+// Base dir to be processed (sent via CLI arguments)
+export const baseDir = process.argv[2] || null;
+// Output directory for JXL files
+export const jxlDir = baseDir ? path.join(baseDir, "jxl") : null;
+// Destination directory for original files
+export const origDir = baseDir ? path.join(baseDir, "orig") : null;
+
+// Number of spaces to pad file sizes in summary
+export const padding = 12;
+// Number of places to show on file sizes in summary
+export const places = 1;
+
+export default {
+  baseDir,
+  padding,
+  places,
+  jxlDir,
+  origDir,
+};
