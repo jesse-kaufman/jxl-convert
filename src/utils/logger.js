c77609779dc942ebc8cd98d7c85819d883dc15e6
Jesse Kaufman <jesse@jessekaufman.com>
2024-10-23 18:10:55 -0500

    Add support for unlimited arguments

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,50 +2,52 @@ export default {
   /**
    * Logs a success message to the console in green color.
    *
-   * @param {string} msg The message to be logged.
+   * @param {any} msgs The message(s) to be logged.
    * @returns {void}
    */
-  success: (msg) => console.log(`\x1b[1m\x1b[32m${msg}\x1b[0m`),
+  success: (...msgs) =>
+    console.log(`\x1b[1m\x1b[32m${msgs.shift()}\x1b[0m`, ...msgs),
 
   /**
    * Logs an error message to the console in red color.
    *
-   * @param {string} msg The message to be logged.
+   * @param {any} msgs The message(s) to be logged.
    * @returns {void}
    */
-  error: (msg) => console.error(`\x1b[1m\x1b[31m${msg}\x1b[0m`),
+  error: (...msgs) =>
+    console.error(`\x1b[1m\x1b[31m${msgs.shift()}\x1b[0m`, ...msgs),
 
   /**
    * Logs a warning message to the console in yellow color.
    *
-   * @param {string} msg The message to be logged.
+   * @param {any} msgs The message(s) to be logged.
    * @returns {void}
    */
-  warn: (msg) => console.warn(`\x1b[33m${msg}\x1b[0m`),
+  warn: (...msgs) => console.warn(`\x1b[33m${msgs.shift()}\x1b[0m`, ...msgs),
 
   /**
    * Logs a message to the console.
    *
-   * @param {string} msg The message to be logged.
+   * @param {any} msgs The message(s) to be logged.
    * @returns {void}
    */
-  info: (msg) => console.info(msg),
+  info: (...msgs) => console.info(...msgs),
 
   /**
    * Logs a highlighted message to the console.
    *
-   * @param {string} msg The message to be logged.
+   * @param {any} msgs The message(s) to be logged.
    * @returns {void}
    */
-  notice: (msg) => console.info(`\x1b[34m${msg}\x1b[0m`),
+  notice: (...msgs) => console.info(`\x1b[34m${msgs.shift()}\x1b[0m`),
 
   /**
    * Logs a debug message to the console if debugging is enabled.
    *
-   * @param {string} msg The message to be logged.
+   * @param {any} msgs The message(s) to be logged.
    * @returns {void|null}
    */
-  debug: (msg) => {
-    if (process.env.DEBUG) console.debug(`[DEBUG] ${msg}`);
+  debug: (...msgs) => {
+    if (process.env.DEBUG) console.debug(`[DEBUG] ${msgs.shift()}`, ...msgs);
   },
 };
