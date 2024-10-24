ef561226fb4a54ffbb94cc25204ef165d2899dae
Jesse Kaufman <jesse@jessekaufman.com>
2024-10-23 20:55:27 -0500

    Rework rest parameters

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,46 +6,52 @@ export default {
   /**
    * Logs a success message to the console in green color.
    *
-   * @param {any} msgs The message(s) to be logged.
+   * @param {string} msg - The message.
+   * @param {any} args - Additional arguments.
    */
-  success: (...msgs) =>
-    console.log(`\x1b[1m\x1b[32m${msgs.shift()}\x1b[0m`, ...msgs),
+  success: (msg, ...args) =>
+    console.log(`\x1b[1m\x1b[32m${msg}\x1b[0m`, ...args),
 
   /**
    * Logs an error message to the console in red color.
    *
-   * @param {any} msgs The message(s) to be logged.
+   * @param {string} msg - The message.
+   * @param {any} args - Additional arguments.
    */
-  error: (...msgs) =>
-    console.error(`\x1b[1m\x1b[31m${msgs.shift()}\x1b[0m`, ...msgs),
+  error: (msg, ...args) =>
+    console.error(`\x1b[1m\x1b[31m${msg}\x1b[0m`, ...args),
 
   /**
    * Logs a warning message to the console in yellow color.
    *
-   * @param {any} msgs The message(s) to be logged.
+   * @param {string} msg - The message.
+   * @param {any} args - Additional arguments.
    */
-  warn: (...msgs) => console.warn(`\x1b[33m${msgs.shift()}\x1b[0m`, ...msgs),
+  warn: (msg, ...args) => console.warn(`\x1b[33m${msg}\x1b[0m`, ...args),
 
   /**
    * Logs a message to the console.
    *
-   * @param {any} msgs The message(s) to be logged.
+   * @param {string} msg - The message.
+   * @param {any} args - Additional arguments.
    */
-  info: (...msgs) => console.info(...msgs),
+  info: (msg, ...args) => console.info(msg, ...args),
 
   /**
    * Logs a highlighted message to the console.
    *
-   * @param {any} msgs The message(s) to be logged.
+   * @param {string} msg - The message.
+   * @param {any} args - Additional arguments.
    */
-  notice: (...msgs) => console.info(`\x1b[34m${msgs.shift()}\x1b[0m`),
+  notice: (msg, ...args) => console.info(`\x1b[34m${msg}\x1b[0m`, ...args),
 
   /**
    * Logs a debug message to the console if debugging is enabled.
    *
-   * @param {any} msgs The message(s) to be logged.
+   * @param {string} msg - The message.
+   * @param {any} args - Additional arguments.
    */
-  debug: (...msgs) => {
-    if (process.env.DEBUG) console.debug(`[DEBUG] ${msgs.shift()}`, ...msgs);
+  debug: (msg, ...args) => {
+    if (process.env.DEBUG) console.debug(`[DEBUG] ${msg}`, ...args);
   },
 };
