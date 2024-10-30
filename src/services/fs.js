64607cc0b5596d3876378af2e70a312d432dde02
Jesse Kaufman <jesse@jessekaufman.com>
2024-10-30 17:39:34 -0500

    Make asynchronous

--- src/services/fs.js
+++ src/services/fs.js
@@ -1,24 +1,27 @@
-import fs from "fs";
+import fsp from "node:fs/promises";
 import log from "./logger/logger.js";
 
 /**
  * Creates a directory if it doesn't exist already.
  * @param {string|null} dir - The directory to create.
+ * @returns {Promise<void>} Promise.
  */
-export const createDir = (dir) => {
+export const createDir = async (dir) => {
   if (!dir) return;
 
   // Try to create the directory
   log.debug(`Creating ${dir} directory...`);
-  fs.mkdir(dir, (err) => {
-    // Successfully created directory
-    if (err == null) return log.success(`${dir} created successfully`);
-
+  try {
+    await fsp.mkdir(dir);
+  } catch (err) {
     // Directory already existed
     if (err?.code === "EEXIST") return log.success(`${dir} already exists`);
 
     // An error occurred
     log.error(err.message);
     process.exit(1);
-  });
+  }
+
+  // Successfully created directory
+  log.success(`${dir} created successfully`);
 };
